40faa5f78d59eec5d294242f843344e5d045131a
K <ksaynice@gmail.com>
2013-04-08 11:07:42 -0700

    gs='git status'

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -1,8 +1,8 @@
 # Aliases
 alias g='git'
 compdef g=git
-alias gst='git status'
-compdef _git gst=git-status
+alias gs='git status'
+compdef _git gs=git-status
 alias gd='git diff'
 compdef _git gd=git-diff
 alias gl='git pull'
