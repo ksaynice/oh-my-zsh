e5dffef3cb82e0a1a4cef3ea8d32d6d7aab7f679
K <ksaynice@gmail.com>
2013-06-20 13:27:03 -0700

    disable auto correct for git commands

--- lib/correction.zsh
+++ lib/correction.zsh
@@ -6,5 +6,12 @@ if [[ "$ENABLE_CORRECTION" == "true" ]]; then
   alias sudo='nocorrect sudo'
   alias su='nocorrect su'
 
-  setopt correct_all
-fi
+alias man='nocorrect man'
+alias mv='nocorrect mv'
+alias mysql='nocorrect mysql'
+alias mkdir='nocorrect mkdir'
+alias gist='nocorrect gist'
+alias heroku='nocorrect heroku'
+alias ebuild='nocorrect ebuild'
+alias hpodder='nocorrect hpodder'
+alias git='nocorrect git'
